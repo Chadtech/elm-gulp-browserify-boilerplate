8d724f10976b3c1ad13a53c98b4541403a0a9796
Chadtech <chadstearns1@gmail.com>
2017-02-16 21:00:50 -0500

    ALL THE FILES

---
+++ source/Main.elm
@@ -0,0 +1,47 @@
+module Main exposing (..)
+
+import Html
+import Types exposing (..)
+import Ports exposing (..)
+import View.Main exposing (view)
+
+
+-- MAIN
+
+
+main =
+    Html.program
+        { init = ( Model "", Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    fromJS HandlePort
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+    case message of
+        UpdateField str ->
+            Model str ! []
+
+        CheckIfEnter code ->
+            if code == 13 then
+                Model "Submitted!" ! []
+            else
+                model ! []
+
+        HandlePort str ->
+            Model str ! []
